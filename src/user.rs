1642036cc7547478523750ed744e1588f3491a2f
Karosia <ofkarosia@gmail.com>
2026-06-20 16:12:49 +0800

    fix: cache user and repo_dir

--- src/user.rs
+++ src/user.rs
@@ -1,21 +1,32 @@
 use anyhow::Result;
-use std::{path::PathBuf, process::Command};
+use once_cell::sync::OnceCell;
+use std::{path::{Path, PathBuf}, process::Command};
 
 use crate::extension::CommandExt;
 
-pub const REPO_DIR: &str = "./.sfm/repo";
+const REPO_REL_PATH: &str = "./.sfm/repo";
+static USER: OnceCell<String> = OnceCell::new();
+static REPO_DIR: OnceCell<PathBuf> = OnceCell::new();
 
-pub fn get_user() -> Result<String> {
+fn whoami() -> Result<String> {
     Command::new("whoami").output_checked()
+}
+
+pub fn get_user() -> Result<&'static str> {
+    USER.get_or_try_init(whoami).map(|s| s.as_str())
 }
 
 pub fn is_root() -> Result<bool> {
     get_user().map(|s| s.trim() == "root")
 }
 
-pub fn get_repo_dir() -> Result<PathBuf> {
+fn gen_repo_dir() -> Result<PathBuf> {
     let mut path = PathBuf::from("/home");
     path.push(get_user()?.trim());
-    path.push(REPO_DIR);
+    path.push(REPO_REL_PATH);
     Ok(path)
+}
+
+pub fn get_repo_dir() -> Result<&'static Path> {
+    REPO_DIR.get_or_try_init(gen_repo_dir).map(|d| d.as_path())
 }
